fded19f53d697f3cc79c34cc685324d4296301da
rishpatil123 <rishpatil123@gmail.com>
2022-11-07 17:42:53 +0530

    done

--- GitTestProject/src/main/java/com/gitTest/controller/Rish.java
+++ GitTestProject/src/main/java/com/gitTest/controller/Rish.java
@@ -5,6 +5,7 @@ public class Rish {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("well to GitHub Project");
+System.out.println("hey java");
 	}
 
 }
