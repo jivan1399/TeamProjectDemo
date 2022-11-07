8468271f545766ef8fb1f01668f44ec8e9a7e09c
bhagwat-ghodke <Bandgar9960@gmail.com>
2022-11-07 18:57:51 +0530

    Add UserController

---
+++ GitTestProject/src/main/java/com/gitTest/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.gitTest.controller;
+
+public class UserController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("well to GitHub Project");
+System.out.println("hey java");
+	}
+
+}
