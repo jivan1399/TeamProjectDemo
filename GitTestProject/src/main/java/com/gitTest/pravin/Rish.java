eb022708f92fff34cf0bd238cea2925856556e4c
rishpatil123 <rishpatil123@gmail.com>
2022-11-07 17:22:21 +0530

    updated

---
+++ GitTestProject/src/main/java/com/gitTest/pravin/Rish.java
@@ -0,0 +1,11 @@
+package com.gitTest.pravin;
+
+public class Rish {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("well Come To My Club");
+System.out.println(" My 1st Git Project");
+	}
+
+}
